85026e55635ef99a885a5d3ebf7f982e9a807da4
Toni Janßen <tonijanssen1992@hotmail.com>
2026-06-13 16:58:57 +0200

    v2: Multi-Target-Scan via verschachtelte Schleifen

--- port_scanner.py
+++ port_scanner.py
@@ -1,13 +1,13 @@
 import socket
-ziel = "192.168.0.80"
+zielliste = ("<erste Ziel IP>", "<zweite Ziel IP>")
 portliste = (22, 23, 25, 53, 67, 68, 80, 110, 123, 143, 443, 445 ) #ein tuple, unveränderbar im Prozess
 
-
-for port in portliste:
-    fStringOffen = f"Portnummer {port} ist offen." #fString = formattierter String
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1.5) #socket timeout nach 1.5 sek
-    ergebnis = s.connect_ex((ziel, port)) # 0 = offen, sonst Fehlercode (errno) = zu/gefilter
-    if ergebnis == 0:
-        print(fStringOffen)
-    s.close()
+for ziel in zielliste:
+    for port in portliste:
+        fStringOffen = f"Ziel-IP: {ziel} mit der Portnummer {port} ist offen." #fString = formattierter String
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1.5) #socket timeout nach 1.5 sek
+        ergebnis = s.connect_ex((ziel, port)) # 0 = offen, sonst Fehlercode (errno) = zu/gefilter
+        if ergebnis == 0:
+            print(fStringOffen)
+        s.close()
